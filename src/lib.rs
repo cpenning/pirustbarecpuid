c15966f24ee19fdd94674d93727f89575e1625f3
Ubuntu <ubuntu@osdev.lxd>
2018-02-19 19:15:26 +0000

    Check that the external function definition compiles

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
 #![no_std]
 
+extern {
+    fn GETCPUID() -> u32;
+}
+
 #[no_mangle]
-pub extern fn notmain() {}
+pub extern fn notmain() {
+    let id: u32;
+    unsafe {
+        id = GETCPUID();
+    }
+}
